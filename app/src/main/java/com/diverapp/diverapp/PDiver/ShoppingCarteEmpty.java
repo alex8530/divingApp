fad5af91b1269f9f72447be20cf2d39a57c04a46
Alex <Aliabuharb8530@gmil.com>
2018-12-27 23:57:01 +0200

    upadate

--- app/src/main/java/com/diverapp/diverapp/PDiver/ShoppingCarteEmpty.java
+++ app/src/main/java/com/diverapp/diverapp/PDiver/ShoppingCarteEmpty.java
@@ -1,7 +1,13 @@
 package com.diverapp.diverapp.PDiver;
 
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.diverapp.diverapp.R;
 
@@ -10,6 +16,7 @@ public class ShoppingCarteEmpty extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_shopping_carte_empty);
     }
 }
