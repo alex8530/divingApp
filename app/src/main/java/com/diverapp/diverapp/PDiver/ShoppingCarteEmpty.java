1cc37d7da1a5558598e11ee193423b6b0e262763
Alex <Aliabuharb8530@gmil.com>
2018-12-27 23:57:32 +0200

    Edit Status bar color

--- app/src/main/java/com/diverapp/diverapp/PDiver/ShoppingCarteEmpty.java
+++ app/src/main/java/com/diverapp/diverapp/PDiver/ShoppingCarteEmpty.java
@@ -12,11 +12,21 @@ import android.view.WindowManager;
 import com.diverapp.diverapp.R;
 
 public class ShoppingCarteEmpty extends AppCompatActivity {
-
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public static void setStatusBarGradiant(Activity activity) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = activity.getWindow();
+            Drawable background = activity.getResources().getDrawable(R.drawable.bk_button_shape);
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(activity.getResources().getColor(android.R.color.transparent));
+            window.setNavigationBarColor(activity.getResources().getColor(android.R.color.transparent));
+            window.setBackgroundDrawable(background);
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        setStatusBarGradiant(this);
         setContentView(R.layout.activity_shopping_carte_empty);
     }
 }
