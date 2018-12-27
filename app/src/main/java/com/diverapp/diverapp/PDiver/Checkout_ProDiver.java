03d4d51d08b331cf78486089cc4b53e9763020dc
Alex <Aliabuharb8530@gmil.com>
2018-12-27 16:15:06 +0200

    add shopping cart

--- app/src/main/java/com/diverapp/diverapp/PDiver/Checkout_ProDiver.java
+++ app/src/main/java/com/diverapp/diverapp/PDiver/Checkout_ProDiver.java
@@ -14,12 +14,12 @@ import com.diverapp.diverapp.R;
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link Checkout_ProDiverFragment.OnFragmentInteractionListener} interface
+ * {@link Checkout_ProDiver.OnFragmentInteractionListener} interface
  * to handle interaction events.
- * Use the {@link Checkout_ProDiverFragment#newInstance} factory method to
+ * Use the {@link Checkout_ProDiver#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class Checkout_ProDiverFragment extends Fragment {
+public class Checkout_ProDiver extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -31,7 +31,7 @@ public class Checkout_ProDiverFragment extends Fragment {
 
     private OnFragmentInteractionListener mListener;
 
-    public Checkout_ProDiverFragment() {
+    public Checkout_ProDiver() {
         // Required empty public constructor
     }
 
@@ -41,11 +41,11 @@ public class Checkout_ProDiverFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment Checkout_ProDiverFragment.
+     * @return A new instance of fragment Checkout_ProDiver.
      */
     // TODO: Rename and change types and number of parameters
-    public static Checkout_ProDiverFragment newInstance(String param1, String param2) {
-        Checkout_ProDiverFragment fragment = new Checkout_ProDiverFragment();
+    public static Checkout_ProDiver newInstance(String param1, String param2) {
+        Checkout_ProDiver fragment = new Checkout_ProDiver();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -66,7 +66,7 @@ public class Checkout_ProDiverFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_f_checkout__pro_diver, container, false);
+        return inflater.inflate(R.layout.layout_f_checkout__pro_diver, container, false);
     }
 
     // TODO: Rename method, update argument and hook method into UI event
