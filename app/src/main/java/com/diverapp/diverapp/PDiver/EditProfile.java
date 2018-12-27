122febed881077f785ada3f496a4c00591ffd38b
Alex <Aliabuharb8530@gmil.com>
2018-12-27 18:20:34 +0200

    add activity edit profile

---
+++ app/src/main/java/com/diverapp/diverapp/PDiver/EditProfile.java
@@ -0,0 +1,23 @@
+package com.diverapp.diverapp.PDiver;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import com.diverapp.diverapp.R;
+
+public class EditProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_profile);
+
+        Spinner s1 =   findViewById(R.id.spinner);
+
+
+        ArrayAdapter adapter = ArrayAdapter.createFromResource(this, R.array.country_arrays, R.layout.row_spinner_item);
+        s1.setAdapter(adapter);
+    }
+}
